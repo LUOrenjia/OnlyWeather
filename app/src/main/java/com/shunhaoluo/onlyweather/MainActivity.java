42c1abb3a99156e76a297cf113c911b9786e8517
luosh <luosh@hxsmart.com>
2016-05-31 17:41:02 +0800

    更新布局

--- app/src/main/java/com/shunhaoluo/onlyweather/MainActivity.java
+++ app/src/main/java/com/shunhaoluo/onlyweather/MainActivity.java
@@ -1,13 +1,55 @@
 package com.shunhaoluo.onlyweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private ListView listView ;
+    private List<String> mDatas = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.iso_weather_bottom_id);
+        initData();
+//        final View decorView = getWindow().getDecorView();
+//        decorView.setOnSystemUiVisibilityChangeListener
+//                (new View.OnSystemUiVisibilityChangeListener() {
+//                    @Override
+//                    public void onSystemUiVisibilityChange(int visibility) {
+//                        if ((visibility & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0) {
+//                            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//                        } else {
+//                        }
+//                    }
+//                });
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+    public void initData(){
+        for (int i = 0; i < 50; i++)
+        {
+            mDatas.add("mTitle -> " + i);
+        }
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.item, R.id.id_info, mDatas)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+            {
+                return super.getView(position, convertView, parent);
+            }
+        });
     }
 }
